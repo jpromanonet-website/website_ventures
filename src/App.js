7bc9396acb41069743bbe6cdb08f5c0cd4475760
jpromanonet <juanpablorom@gmail.com>
2025-09-25 19:43:22 -0300

    Ventures add

--- src/App.js
+++ src/App.js
@@ -336,6 +336,7 @@ function App() {
                   fullWidth
                 >
                   <MenuItem value="All">All Years</MenuItem>
+                  <MenuItem value="2026">2026</MenuItem>
                   <MenuItem value="2025">2025</MenuItem>
                   <MenuItem value="2024">2024</MenuItem>
                   <MenuItem value="2023">2023</MenuItem>
